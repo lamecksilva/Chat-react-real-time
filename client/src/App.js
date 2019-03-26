5d718b87e3d5d26934cb4624a83f9f743608f03b
Lameck <lameck2@hotmail.com>
2019-03-26 00:27:53 -0300

    First version of chat working :+1: :grin:

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,70 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client';
 
+import './App.css';
+
+let socket;
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      message: '',
+      messages: []
+    };
+  }
+
+  componentDidMount() {
+    socket = io('http://localhost:9000');
+    socket.on('previousMessages', messages => this.setState({ messages }));
+    socket.on('receivedMessage', message =>
+      this.setState({ messages: [...this.state.messages, message] })
+    );
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const { username, message } = this.state;
+
+    if (username.length && message.length) {
+      socket.emit('sendMessage', { author: username, message });
+    }
+  };
+
   render() {
     return (
       <div className="App">
-        <h1>Chat App</h1>
+        <form id="chat" onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="username"
+            placeholder="Type your username"
+            onChange={this.handleChange}
+          />
+          <div className="messages">
+            {this.state.messages.map(msg => (
+              <div className="message">
+                <strong>{msg.author}: </strong>
+                {msg.message}{' '}
+              </div>
+            ))}
+          </div>
+          <input
+            type="text"
+            name="message"
+            placeholder="Type your message"
+            onChange={this.handleChange}
+          />
+          <button type="submit">Send Message</button>
+        </form>
       </div>
     );
   }
--- index.js
+++ index.js
@@ -3,8 +3,19 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+let messages = [];
+
 io.on('connection', socket => {
   console.log(`socket: ${socket.id}`);
+
+  socket.emit('previousMessages', messages);
+
+  socket.on('sendMessage', data => {
+    messages.push(data);
+
+    socket.emit('receivedMessage', data);
+    socket.broadcast.emit('receivedMessage', data);
+  });
 });
 
 const PORT = process.env.PORT || 9000;
