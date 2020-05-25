808be60f3b8688303230ad372e278e3bcf7697c3
lamecksilva <lameck2@hotmail.com>
2020-05-25 10:12:58 -0300

    FIX: Dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -6,132 +6,131 @@ import Shot from './shot.mp3';
 
 let socket;
 class App extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      username: '',
-      message: '',
-      messages: []
-    };
-  }
+		this.state = {
+			username: '',
+			message: '',
+			messages: [],
+		};
+	}
 
-  audio = new Audio(
-    // 'https://notificationsounds.com/soundfiles/1728efbda81692282ba642aafd57be3a/file-sounds-1101-plucky.mp3'
-    Shot
-  );
+	audio = new Audio(
+		// 'https://notificationsounds.com/soundfiles/1728efbda81692282ba642aafd57be3a/file-sounds-1101-plucky.mp3'
+		Shot
+	);
 
-  play() {
-    this.audio.play();
-  }
+	play() {
+		this.audio.play();
+	}
 
-  componentDidMount() {
-    socket = io('http://192.168.25.10:9000');
-    socket.on('previousMessages', async messages => {
-      await messages.sort((a, b) => new Date(a.date) - new Date(b.date));
-      console.log(messages);
-      await this.setState({ messages });
-    });
+	componentDidMount() {
+		socket = io('http://192.168.100.9:5000');
+		socket.on('previousMessages', async (messages) => {
+			await messages.sort((a, b) => new Date(a.date) - new Date(b.date));
+			this.setState({ messages });
+		});
 
-    socket.on('receivedMessage', async message => {
-      console.log(message);
-      this.play();
-      this.setState({ messages: [...this.state.messages, message] });
-      this.scrollToBottom();
-    });
-  }
+		socket.on('receivedMessage', async (message) => {
+			console.log(message);
+			this.play();
+			this.setState({ messages: [...this.state.messages, message] });
+			this.scrollToBottom();
+		});
+	}
 
-  handleChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  };
+	handleChange = (event) => {
+		this.setState({
+			[event.target.name]: event.target.value,
+		});
+	};
 
-  handleSubmit = event => {
-    event.preventDefault();
-    console.log(this.state);
+	handleSubmit = (event) => {
+		event.preventDefault();
+		console.log(this.state);
 
-    const { username, message } = this.state;
+		const { username, message } = this.state;
 
-    if (username.length && message.length) {
-      socket.emit('sendMessage', { author: username, message: message });
-      this.setState({ message: '' });
-    }
-  };
+		if (username.length && message.length) {
+			socket.emit('sendMessage', { author: username, message: message });
+			this.setState({ message: '' });
+		}
+	};
 
-  scrollToBottom() {
-    const scrollHeight = this.messageList.scrollHeight;
-    const height = this.messageList.clientHeight;
-    const maxScrollTop = scrollHeight - height;
-    this.messageList.scrollTop = maxScrollTop > 0 ? maxScrollTop : 0;
-  }
+	scrollToBottom() {
+		const scrollHeight = this.messageList.scrollHeight;
+		const height = this.messageList.clientHeight;
+		const maxScrollTop = scrollHeight - height;
+		this.messageList.scrollTop = maxScrollTop > 0 ? maxScrollTop : 0;
+	}
 
-  render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 d-flex col-xs-12">
-            <div className="mx-auto col-md-8">
-              <h4 className="text-center mt-3">Chat App</h4>
-              <form onSubmit={this.handleSubmit} className="col-xs-12">
-                <div className="form-group col-xs-12">
-                  <label htmlFor="usernameInput">Username</label>
-                  <input
-                    name="username"
-                    id="usernameInput"
-                    className="form-control"
-                    type="text"
-                    onChange={this.handleChange}
-                    placeholder="Enter your username"
-                  />
-                </div>
+	render() {
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col-md-12 d-flex col-xs-12">
+						<div className="mx-auto col-md-8">
+							<h4 className="text-center mt-3">Chat App</h4>
+							<form onSubmit={this.handleSubmit} className="col-xs-12">
+								<div className="form-group col-xs-12">
+									<label htmlFor="usernameInput">Username</label>
+									<input
+										name="username"
+										id="usernameInput"
+										className="form-control"
+										type="text"
+										onChange={this.handleChange}
+										placeholder="Enter your username"
+									/>
+								</div>
 
-                <div
-                  ref={div => {
-                    this.messageList = div;
-                  }}
-                  className="list-group list-group-flush messages"
-                >
-                  {this.state.messages.map(msg => (
-                    <div key={msg._id} className="list-group-item">
-                      <small>
-                        {new Date(msg.date).getHours()}:
-                        {new Date(msg.date).getMinutes()}
-                      </small>
-                      {'  '}
-                      <strong>{msg.author}: </strong>
-                      {msg.message}
-                      {'  '}
-                    </div>
-                  ))}
-                </div>
+								<div
+									ref={(div) => {
+										this.messageList = div;
+									}}
+									className="list-group list-group-flush messages"
+								>
+									{this.state.messages.map((msg) => (
+										<div key={msg._id} className="list-group-item">
+											<small>
+												{new Date(msg.date).getHours()}:
+												{new Date(msg.date).getMinutes()}
+											</small>
+											{'  '}
+											<strong>{msg.author}: </strong>
+											{msg.message}
+											{'  '}
+										</div>
+									))}
+								</div>
 
-                <div className="form-group">
-                  <label htmlFor="messageInput">Message</label>
-                  <input
-                    type="input"
-                    name="message"
-                    value={this.state.message}
-                    placeholder="Type your message"
-                    onChange={this.handleChange}
-                    className="form-control"
-                  />
-                </div>
+								<div className="form-group">
+									<label htmlFor="messageInput">Message</label>
+									<input
+										type="input"
+										name="message"
+										value={this.state.message}
+										placeholder="Type your message"
+										onChange={this.handleChange}
+										className="form-control"
+									/>
+								</div>
 
-                <button
-                  type="submit"
-                  className="btn btn-primary btn-lg btn-block mb-3"
-                  onClick={this.handleSubmit}
-                >
-                  Send Message
-                </button>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+								<button
+									type="submit"
+									className="btn btn-primary btn-lg btn-block mb-3"
+									onClick={this.handleSubmit}
+								>
+									Send Message
+								</button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
--- index.js
+++ index.js
@@ -7,42 +7,40 @@ const mongoose = require('mongoose');
 const Message = require('./models/Message');
 
 mongoose
-  .connect('mongodb://localhost:27017/real-time-chat', {
-    useNewUrlParser: true
-  })
-  .then(result => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+	.connect('mongodb://localhost:27017/real-time-chat', {
+		useNewUrlParser: true,
+	})
+	.then(() => console.log('MongoDB Connected'))
+	.catch((err) => console.log(err));
 
 const getMessages = async () => {
-  const messages = await Message.find({}, {}, { sort: { date: -1 } }).limit(10);
+	const messages = await Message.find({}, {}, { sort: { date: -1 } }).limit(10);
 
-  await messages.map(message => {
-    return {
-      ...message._doc
-    };
-  });
-
-  return messages;
+	return messages.map((message) => {
+		return {
+			...message._doc,
+		};
+	});
 };
 
-io.on('connection', async socket => {
-  console.log(`socket: ${socket.id}`);
+io.on('connection', async (socket) => {
+	console.log(`socket: ${socket.id}`);
 
-  socket.emit('previousMessages', await getMessages());
+	socket.emit('previousMessages', await getMessages());
 
-  socket.on('sendMessage', data => {
-    new Message(data)
-      .save()
-      .then(result => {
-        console.log(result);
+	socket.on('sendMessage', (data) => {
+		new Message(data)
+			.save()
+			.then((result) => {
+				console.log(result);
 
-        socket.emit('receivedMessage', result);
-        socket.broadcast.emit('receivedMessage', result);
-      })
-      .catch(err => console.log(err));
-  });
+				socket.emit('receivedMessage', result);
+				socket.broadcast.emit('receivedMessage', result);
+			})
+			.catch((err) => console.log(err));
+	});
 });
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
